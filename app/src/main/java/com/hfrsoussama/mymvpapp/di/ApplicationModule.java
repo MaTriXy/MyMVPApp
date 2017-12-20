6c5c8935dd56815997e1dfd80dfe41c7be8ba83b
Oussama <hfrs.oussama@gmail.com>
2017-12-20 21:08:31 +0100

    added Networking Module

---
+++ app/src/main/java/com/hfrsoussama/mymvpapp/di/ApplicationModule.java
@@ -0,0 +1,34 @@
+package com.hfrsoussama.mymvpapp.di;
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Oussama on 20/12/2017.
+ */
+
+@Module
+public class ApplicationModule {
+
+    Application mApplication;
+
+    public ApplicationModule(Application application) {
+        mApplication = application;
+    }
+
+    @Provides
+    @Singleton
+    Application providesApplication(){
+        return mApplication;
+    }
+
+    @Provides
+    Context providesContext(){
+        return mApplication;
+    }
+}
---
+++ app/src/main/java/com/hfrsoussama/mymvpapp/di/NetworkingModule.java
@@ -0,0 +1,71 @@
+package com.hfrsoussama.mymvpapp.di;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Oussama on 20/12/2017.
+ */
+
+@Module
+public class NetworkingModule {
+
+    private String mBaseUrl;
+
+    public NetworkingModule(String baseUrl) {
+        mBaseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences provideSharedPreferences(Application application) {
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+    @Provides
+    @Singleton
+    Cache provideOkHttpCache(Application application) {
+        int cacheSize = 10 * 1024 * 1024;
+         return new Cache(application.getCacheDir(), cacheSize);
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        return new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+                .create();
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache) {
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(mBaseUrl)
+                .client(okHttpClient)
+                .build();
+    }
+}
